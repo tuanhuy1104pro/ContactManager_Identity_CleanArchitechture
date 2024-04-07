3ce7edf004d67651455c774380ddbf18c4264285
tuanhuy1104pro@gmail.com <tuanhuy1104pro@gmail.com>
2024-04-07 14:49:52 +0700

    Access Register Controller got error when inject ManagerUser

--- BaseProject_DatabaseBeOn_CleanArchitecture/Controllers/AccountController.cs
+++ BaseProject_DatabaseBeOn_CleanArchitecture/Controllers/AccountController.cs
@@ -1,22 +1,52 @@
 ﻿using BaseProject_DatabaseBeOn.Controllers;
+using CoreLayer.Domain.IdentityEntities;
 using CoreLayer.DTO;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BaseProject_DatabaseBeOn_CleanArchitecture.Controllers
-{
+{   
     [Route("[Controller]/[Action]")]
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _usermanager;
+        public AccountController(UserManager<ApplicationUser> usermanager)
+        {
+            _usermanager = usermanager;
+        }
         [HttpGet]
-        public IActionResult Register()
+        public async Task<IActionResult> Register()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Register(RegisterDTO registerDTO)
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
         {
-            return RedirectToAction(nameof(PersonsController.Index),"Persons");
+            if(ModelState.IsValid == false)
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(temp => temp.Errors).Select(temp => temp.ErrorMessage);
+                return View(registerDTO);
+            }
+            ApplicationUser user = new ApplicationUser() {Email = registerDTO.Email,PhoneNumber = registerDTO.Phone,UserName = registerDTO.Email,PersonName = registerDTO.PersonName };
+            IdentityResult result =  await _usermanager.CreateAsync(user,registerDTO.Password);
+            //IdentityResult contain status of identity
+            
+            if(result.Succeeded)
+            {
+                return RedirectToAction(nameof(PersonsController.Index), "Persons");
+            }
+            else
+            {
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("Register",error.Description);
+                }
+                return View(registerDTO);
+            }
+            // Store usser registration details into Identity database
+
+           
         }
     }
 }
